c9596dcc9ff691b1dd36535719c58f531327af12
Fourgees <45925475+Fourgees@users.noreply.github.com>
2022-06-15 11:51:17 -0500

    joystick_example.py

--- examples/joystick.py
+++ examples/joystick.py
@@ -1,18 +1,49 @@
 import uinput
+from inputs import get_gamepad
 
 def main():
+    # What events can proc on the virtual xbox controller
     events = (
-        uinput.BTN_JOYSTICK,
-        uinput.ABS_X + (0, 255, 0, 0),
-        uinput.ABS_Y + (0, 255, 0, 0),
-        )
-
-    with uinput.Device(events) as device:
-        for i in range(20):
-            # syn=False to emit an "atomic" (5, 5) event.
-            device.emit(uinput.ABS_X, 5, syn=False)
-            device.emit(uinput.ABS_Y, 5)
-        device.emit_click(uinput.BTN_JOYSTICK)
+    uinput.BTN_A,
+    uinput.BTN_B,
+    uinput.BTN_X,
+    uinput.BTN_Y,
+    uinput.BTN_TL,
+    uinput.BTN_TR,
+    uinput.BTN_THUMBL,
+    uinput.BTN_THUMBR,
+    uinput.ABS_X + (0, 255, 0, 0),
+    uinput.ABS_Y + (0, 255, 0, 0),)
+    
+    #Intialize virtual joystick
+    device = uinput.Device(
+    events,
+    vendor=0x045e,
+    product=0x028e,
+    version=0x110,
+    name="Microsoft X-Box 360 pad",)
+    
+    # Give the device a second to initialize
+    time.sleep(1)
+    # Center Joystick
+    device.emit(uinput.ABS_X, 128, syn=False)
+    device.emit(uinput.ABS_Y, 128)
+    
+    while True:
+        time.sleep(1)
+        device.emit(uinput.ABS_Y, 255)  # Max Y
+        print("Max y")
+        time.sleep(1)
+        #device.emit(uinput.ABS_Y, 0)  # Zero Y
+        device.emit(uinput.ABS_X,255)
+        print("0 Y")
+        print("Sleep for 2 seconds")
+        time.sleep(1)
+        device.emit(uinput.BTN_A, 1)
+        print("Press a")
+        time.sleep(1)
+        print("Release a")
+        device.emit(uinput.BTN_A, 0)
 
 if __name__ == "__main__":
     main()
